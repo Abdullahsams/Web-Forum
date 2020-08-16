d88fba7b53d8f7b4caf873dd3eb8276b93a045c1
Abdullahsams <solid00mail@gmail.com>
2020-08-16 19:03:29 +0700

    Remove: annoying char

--- resources/views/adminlte/partials/sidebar.blade.php
+++ resources/views/adminlte/partials/sidebar.blade.php
@@ -1,6 +1,6 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="{{asset('adminlte/index3.html')}}" class="brand-link">
+    <a href="/" class="brand-link">
       <img src="{{asset('adminlte/dist/img/AdminLTELogo.png')}}"
            alt="AdminLTE Logo"
            class="brand-image img-circle elevation-3"
@@ -22,7 +22,7 @@
           </div>
       </div>
 
-      <div class="user-panel mt-3 pb-3 mb-3 d-flex">>      
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <li class="nav-item">
             <a href="#" class="nav-link">
