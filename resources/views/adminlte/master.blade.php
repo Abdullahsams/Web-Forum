16c67886dba982a5af447b87a59b0c127245c46d
Harry Sams <solid00mail@gmail.com>
2020-08-16 17:17:51 +0700

    Add word break property

--- resources/views/adminlte/master.blade.php
+++ resources/views/adminlte/master.blade.php
@@ -15,6 +15,9 @@
   <link rel="stylesheet" href="{{asset('adminlte/dist/css/adminlte.min.css')}}">
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+  
+  <!-- Word break -->
+  <link rel="stylesheet" href="{{asset('adminlte/dist/css/break.css')}}">
 
   @stack('script-head')
 </head>
